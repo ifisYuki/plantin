66b05f43866a48a45540ffdc2f00f41cf62c0f39
ifisyuki <yukizhu@foxmail.com>
2024-03-27 16:59:25 +0100

    setup gh-pages

--- src/Feed.js
+++ src/Feed.js
@@ -15,11 +15,32 @@ function Feed() {
     },
     {
         id: 2,
-        name: 'Yuki',
+        name: 'Nick Desnoyer',
         message: 'Unlocking the beauty of Arabidopsis with genetics🌸Various floral morphs and color variations found in segregants of my hybrid A.thaliana roses. Comment your favorite (A-F) and I will give it an upgrade 🧬',
-        description: 'hi',
-        imageUrl: 'https://yukizhuyue.com/wp-content/uploads/2024/03/4.jpg', // Replace with your image URL
-      },
+        description: 'Researcher and artist interested in live timelapse imaging and synthetic biology in plants 🌱 Exploring cell signaling and development at Universität Zürich🇨🇭',
+        imageUrl: 'https://yukizhuyue.com/wp-content/uploads/2024/03/3.png', // Replace with your image URL
+    },
+    {
+        id: 3,
+        name: 'Christina Rossi, MSc',
+        message: 'The most perfect little Arabidopsis flowers (accession Mr-0) 🌱💚 https://t.co/54cDPPyE1i',
+        description: '@ChristinaRossi_',
+        imageUrl: 'https://pbs.twimg.com/media/GIFKHmFXIAUJdRA?format=jpg&name=medium', // Replace with your image URL
+    },
+    {
+        id: 4,
+        name: 'Nick Desnoyer',
+        message: 'Flower genetics practical - updated with ruby roses to show dihybrid crosses and incomplete dominance 🌸 https://twitter.com/NickDesnoyer/status/1770819274338132248',
+        description: '@NickDesnoyer',
+        imageUrl: 'https://yukizhuyue.com/wp-content/uploads/2024/03/Snipaste_2024-03-26_12-02-49.png', // Replace with your image URL
+    },
+    {
+        id: 5,
+        name: 'Botanical Society of America',
+        message: '🌿Special Issue Call for Papers 🧪 Deadline in 2 weeks! #AppsPlantSci invites proposals for a special issue: "Advances in analyzing & engineering plant metabolic diversity" https://botany.org/home/publications/apps/apps-special-issue-call-for-papers-plant-metabolic-diversity.html ',
+        description: '@Botanical_',
+        imageUrl: 'https://pbs.twimg.com/media/GJhwfSFWkAAIuzb?format=jpg&name=medium', // Replace with your image URL
+    },
   ];
 
   // Pagination state and logic (if you have a lot of posts)
